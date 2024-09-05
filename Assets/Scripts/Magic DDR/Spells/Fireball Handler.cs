29d7cbb3c9cfc72312b17784e3f39dc96deb5aff
Sphyngx <146823394+Sphyngx@users.noreply.github.com>
2024-09-05 21:10:32 +0200

    Completed fireball

---
+++ Assets/Scripts/Magic DDR/Spells/Fireball Handler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballHandler : MonoBehaviour
+{
+    public LayerMask Movable;
+    public LayerMask Damagable;
+    public int ExplotionSize = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        //Knockback on Movable Objects
+        Collider[] HitMovable = Physics.OverlapSphere(gameObject.transform.position, ExplotionSize, Movable);
+        for (int i = 0; i < HitMovable.Length; i++)
+        {
+            Rigidbody ObjectRigidbody = HitMovable[i].GetComponent<Rigidbody>();
+            GameObject Object = HitMovable[i].GetComponent<GameObject>();
+            Vector3 Direction = HitMovable[i].gameObject.transform.position - gameObject.transform.position;
+            ObjectRigidbody.AddForce(Direction * 2 + new Vector3(0,1), ForceMode.Impulse);
+        }
+    }
+}
